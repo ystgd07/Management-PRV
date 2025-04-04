fa36c3af17db76626a2e468dd2e8489c597f6938
yang <ysotgood@gmail.com>
2025-04-04 23:51:26 +0900

    feature: Implement Login Page and Update Main Page Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import OfflineAlert from "./shared/ui/OfflineAlert";
 import UpdatePrompt from "./shared/ui/UpdatePrompt";
-import Main from "./pages/main/Main";
+import LoginPage from "./pages/login/page";
 
 function App() {
   return (
     <div className='mx-auto max-w-[420px] flex flex-col min-h-screen bg-background'>
       <OfflineAlert />
-      <Main />
+      <LoginPage />
       <UpdatePrompt />
     </div>
   );
